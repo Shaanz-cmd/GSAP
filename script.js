da32b0ddfa540efb2c395d84924751faf7d234a4
Shaan <shanali.ozhorik@Gmail.com>
2024-08-14 23:28:15 +0500

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.from(".image", {
   scale: 1.1,
   duration: 1,
-  delay:1.1
+  delay: 1.1,
 });
 gsap.to(".before", {
   x: window.innerWidth,
@@ -25,4 +25,24 @@ gsap.to(".bottom", {
   delay: 1.8,
   padding: 0,
   duration: 1.4,
+});
+gsap.from(".one", {
+  scale: 0,
+  duration: 0.6,
+  delay: 1.4,
+});
+gsap.from(".two", {
+  scale: 0,
+  duration: 0.6,
+  delay: 1.4,
+});
+gsap.from(".three", {
+  scale: 0,
+  duration: 0.6,
+  delay: 1.4,
+});
+gsap.from(".four", {
+  scale: 0,
+  duration: 0.6,
+  delay: 1.4,
 });
