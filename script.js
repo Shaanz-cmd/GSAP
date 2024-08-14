4e97fdf3b62aec80c9b490a854c52cd8ff648052
Shaan <shanali.ozhorik@Gmail.com>
2024-08-14 22:56:37 +0500

    deleted old files,added new ones

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// Make sure to register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
-let tl = gsap.timeline();
-
-gsap.to("#page1 .box", {
-  x: 300,
-  rotate: 360,
-  duration: 3,
+gsap.from(".image", {
+  scale: 1.1,
+  duration: 1,
+  delay:1.1
 });
-gsap.to("#page2 .box", {
-  scrollTrigger: {
-    trigger: "#page2 .box",
-    markers: true,
-    start: "top center",
-    scrub: 1,
-    pin: true,
-  },
-  x: 300,
-  rotate: 360,
-  duration: 3,
+gsap.to(".before", {
+  x: window.innerWidth,
+  duration: 1.5,
+  stagger: true,
+});
+gsap.to(".after", {
+  zIndex: 3,
+  width: window.innerWidth,
+  delay: 4,
+  duration: 1.5,
+});
+gsap.to(".top", {
+  delay: 1.5,
+  padding: 0,
+  duration: 1.4,
+});
+gsap.to(".bottom", {
+  delay: 1.8,
+  padding: 0,
+  duration: 1.4,
 });
